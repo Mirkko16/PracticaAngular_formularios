9280fc8744f72a7048075578282997a16b871f08
Mirkko16 <mirkko1996@hotmail.com>
2022-11-20 20:51:59 -0300

    creacion formularios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'formularios';
+    
+    nombre = 'Nombre...';
+    apellido='Apellido...';
+    direccion='Direccion...';
+    telefono='Tel...';
+
+    getNombre(nombre:string)
+    {
+      return nombre;
+    }
+    getApellido(apellido:string)
+    {
+      return apellido;
+    }
+    getDireccion(direccion:string)
+    {
+      return  direccion;
+    }
+    getTelefono(telefono:string)
+    {
+      return telefono;
+    }
+
+    
+
+
 }
